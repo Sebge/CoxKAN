70bd55c2c04a69e7d3ff2b59e34f1918af904706
Sebastian Germer <56760426+Sebge@users.noreply.github.com>
2024-10-30 13:27:32 +0100

    Update CoxKAN.py

--- coxkan/CoxKAN.py
+++ coxkan/CoxKAN.py
@@ -1022,7 +1022,7 @@ class CoxKAN(KAN):
 
     def load_ckpt(self, ckpt_path, verbose=True):
         ''' Load model from checkpoint '''
-        state = torch.load(ckpt_path, weights_only=False)
+        state = torch.load(ckpt_path, weights_only=True)
         self.load_state_dict(state['state_dict'])
         for k, v in state.items():
             if k != 'state_dict':
